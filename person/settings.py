5d7fe33a1c7c3a779b2c4d18d10ac4b9b7b199dc
CyrusNchege <cyruschegecc@gmail.com>
2023-09-11 12:27:19 +0300

    Add allowed hostes

--- person/settings.py
+++ person/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = [cyrusdjangoapp.azurewebsites.net]
+ALLOWED_HOSTS = ['cyrusdjangoapp.azurewebsites.net']
 
 
 # Application definition
